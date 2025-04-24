e96932566aed22b23aa11cf7834c7a49941bac82
ActiWait <trosio2003@gmail.com>
2025-04-24 11:34:06 +0300

    Результаты расчётов теперь записываются в файл

--- PoroelasticRod.cpp
+++ PoroelasticRod.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
+#include  <fstream>
 #include "NumMethod.h"
 #include "DurbinMethod.h"
-
-std::complex<double> sinus(std::complex<double> s, double x) {
-    std::complex<double> plus1(1,0);
-    return s/(s*s + plus1);
-}
 
 int main() {
     double x = 3;
     double sigma = 5;
-    int N = 2000;
+    int N = 20000;
     double T = 10;
-/*     for (int t = 0; t <= 200; t+=1) {
-        std::cout << "(" << t/100. <<  ",\t" <<  f_N(t/100.*PI, sigma, N, T, x, sinus) << ")" << std::endl;
-    } */
-
-    for (int t = 0; t <= 300; t+=5) {
+    std::ofstream fin;
+    fin.open("Result_Displacement.csv", std::ios::out);
+    for (int t = 0; t <= 300; t+=1) {
         double f = f_N(t/10000., sigma, N, T, x, u_cap);
-        std::cout << "(" << t/1000. << ",\t" << f << ")" << std::endl;
+        fin << t/10000. << ";" << f << std::endl;
     }
+    fin.close();
+
+    x = 0;
+    fin.open("Result_Stress.csv", std::ios::out);
+    for (int t = 0; t <= 300; t+=1) {
+        double f = f_N(t/10000., sigma, N, T, x, p_cap);
+        fin << t/10000. << ";" << f << std::endl;
+    }
+    fin.close();
 
 }
